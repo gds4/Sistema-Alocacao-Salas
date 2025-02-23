7d5cad8d9931e92b8a1f9331e7012a2487afe038
dieglop <diego87dlopes@gmail.com>
2025-02-23 20:04:00 -0300

    adição do swagger em aulas-ms

---
+++ aulas-ms/src/main/java/edu/ifba/aulas_ms/swagger/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package edu.ifba.aulas_ms.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfiguration {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+      .info(new Info()
+        .title("API de Aulas")
+        .version("1.0")
+        .description("Documentação da API de Aulas"));
+  }
+}
